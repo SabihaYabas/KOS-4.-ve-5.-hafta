a985be2647afce89d8cb32e8f60b9d726a6327f9
Sabiha Yabaş <73074301+SabihaYabas@users.noreply.github.com>
2020-11-17 17:59:05 +0200

    kos

---
+++ FormHome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormKOS.Model;
+
+namespace WinFormKOS
+{
+    public partial class FormHome : Form
+    {
+        public FormHome()
+        {
+            InitializeComponent();
+        }
+
+        private void FormHome_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(UserInfo.userId.ToString());
+        }
+
+        private void btnKitapEkle_Click(object sender, EventArgs e)
+        {
+            FormKitapEkle formKitapEkle = new FormKitapEkle();
+            formKitapEkle.Show();
+        }
+
+        private void btnOkuyucuEkle_Click(object sender, EventArgs e)
+        {
+            FormOkuyucuEkle formOkuyucuEkle = new FormOkuyucuEkle();
+            formOkuyucuEkle.Show();
+        }
+
+        private void btnAyarlar_Click(object sender, EventArgs e)
+        {
+            FormEdit formEdit = new FormEdit();
+            formEdit.Show();
+        }
+    }
+}
---
+++ Model/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WinFormKOS.Model
+{
+    class Helper
+    {
+        public static string profilPath(int okuyucuId )
+        {
+            string path = Application.StartupPath + "/profil/" + okuyucuId + ".jpg ";
+            if(File.Exists(path ))
+            {
+                return path;
+            }
+
+            return Application.StartupPath + "/profil/gender-neutral-user";
+        }
+
+
+
+
+
+    }
+}
